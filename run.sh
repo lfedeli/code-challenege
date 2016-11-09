bb699c26ed0075f12a05f300e9220fafb95c654e
lfedeli <livio_fedeli@hotmail.com>
2016-11-09 18:50:51 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 g++ -std=c++11 -o ./src/project ./src/project.cpp
 ./src/project ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
 
+# uncomment these part if you want to copy the outputs
 #cp ./paymo_output/output1.txt ./insight_testsuite/tests/livio-test/paymo_output//output1.txt
 #cp ./paymo_output/output2.txt ./insight_testsuite/tests/livio-test/paymo_output//output2.txt
 #cp ./paymo_output/output3.txt ./insight_testsuite/tests/livio-test/paymo_output//output3.txt
